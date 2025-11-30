de387831ea5b1200a5939827b497868e5516bce4
arham83 <arhamriaz83@gmail.com>
2025-11-30 11:59:32 +0400

    Fixing args and test dir

--- snippets/intelli_generator.py
+++ snippets/intelli_generator.py
@@ -9,9 +9,8 @@ from utils.helper import Helper
 
 
 class IntelliGen():
-    def __init__(self, logger, case_study, budget):
+    def __init__(self, logger, case_study):
         self.log = logger
-        self.budget = budget
         os.makedirs("soi", exist_ok=True) 
         os.makedirs("temp", exist_ok=True)
         os.makedirs("gen_config", exist_ok=True)
@@ -19,7 +18,6 @@ class IntelliGen():
         self.soi = self.init_soi()
         self.seed_gen = SeedGenerator(logger, self.soi, "seeds")
         self.mutator = GenerateMutation(logger, case_study, self.soi)
-        
     
     def init_soi(self):
         """
@@ -37,24 +35,19 @@ class IntelliGen():
         soi = Helper.read_ulg("soi/soi.ulg", 30)
         self.log.info(f"co-ordinates of the SOI: {img_path}")
         return soi
-    
-    def get_Seeds(self, case_study):
-        self.log.info("Generating Seeds...")
-        self.seed_gen.get_seeds(case_study)
 
-    
-    def run(self):
+    def run(self, budget):
         iteration = 0
         seed_iter = 0
         test_dir = set()
+        test_cases = []
         col = ["Iteration", "distance", "time", "obs1-size", "obs1-position", "obs2-size", "obs2-position"]
 
         # Generate the seeds
-        self.get_Seeds(self.case_study)
-        seeds_yaml, seeds_df, uti_budget = self.seed_gen.get_top_seeds(5)
+        seeds_yaml, seeds_df, uti_budget = self.seed_gen.get_seeds(self.case_study, test_cases)
         
-        # Lets run Simulation
-        while (iteration <= (self.budget -uti_budget)):
+        # run Simulation
+        while (iteration <= (budget -uti_budget)):
             sel_yaml = seeds_yaml[seed_iter]
             print(f"Selected Seed: {sel_yaml}")
             self.log.info(f"Selected Seed: {sel_yaml}")
@@ -68,7 +61,7 @@ class IntelliGen():
                     "temp/trajectory.ulg",
                     "temp/mission.yaml",
                     test_dir,
-                    iter=count,
+                    iter=iteration,
                 )
                 Helper.copy_file(test_path, "temp", "mission") 
                 with open(test_path, 'r', encoding='utf-8') as bs:
@@ -81,14 +74,17 @@ class IntelliGen():
                 img_path = test.plot()
                 self.log.info(f"Trajectory of Mutated Config stored at following path: {img_path}")
                 val = Helper.get_config_info(test_path)
+                test_cases.append(test)
                 Helper.write_csv(col, [iteration, min(distances), Helper.get_flight_time(ulg_path), val['obs1_size'], val['obs1_position'],val['obs2_size'], val['obs2_position']],f"results.csv")
                 iteration +=1
                 if min(distances) > 1.5:
                     break
             
             seed_iter +=1
-            if seed_iter == 5:
+            if seed_iter == 6:
                 seed_iter = 0
+
+        return test_cases
 
 from utils.logger import LoggerManager
 logger = LoggerManager(name='UAV Generator',log_dir='logs', level='INFO').get_logger()
